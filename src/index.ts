871c5254cc349bec9cff1192174bde19d3a034f0
AndrewLemons <andrewglemons@gmail.com>
2024-10-07 03:05:42 -0400

    Improve type exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import BambuPrinter from "./classes/BambuPrinter";
 import type { RawPrinterState } from "./types/RawPrinterState";
 import type { PrinterState } from "./types/PrinterState";
-import type { PrintStage } from "./types/PrintStage";
+import { PrintStage, printStageValues } from "./types/PrintStage";
+import convertState from "./utilities/stateConverter";
 import * as errors from "./utilities/errors";
 import * as hms from "./utilities/hms";
 
-export { BambuPrinter, errors, hms };
-export type { PrinterState, PrintStage, RawPrinterState };
+export default BambuPrinter;
+export {
+	BambuPrinter,
+	PrintStage,
+	printStageValues,
+	errors,
+	hms,
+	convertState,
+};
+export type { PrinterState, RawPrinterState };
